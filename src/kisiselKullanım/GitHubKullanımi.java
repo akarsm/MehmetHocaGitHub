298347e5fb5dd7338d463378bd2bb0729b0b4290
Sevgi Akar <sesamu23@gmail.com>
2022-05-15 20:56:43 +0300

    ilkVersion

---
+++ src/kisiselKullanım/GitHubKullanımi.java
@@ -0,0 +1,12 @@
+package kisiselKullanım;
+
+public class GitHubKullanımi {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //1-projeyi git ile irtibatlandırmak için ilk komut "git init" bunu yazınca proje githubla ilişkilendirildi
+        //2-git ile irtibatlandırdıktan sonra benim dosayalarımın git e eklenmesi için "git add .
+        //3-
+        //4-
+
+    }
+}
