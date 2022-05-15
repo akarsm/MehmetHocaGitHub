3fd98d5e498b80bc936cc56fa5f7324638207729
Sevgi Akar <sesamu23@gmail.com>
2022-05-15 21:14:38 +0300

    ilkDegisiklik

--- src/kisiselKullanım/GitHubKullanımi.java
+++ src/kisiselKullanım/GitHubKullanımi.java
@@ -3,10 +3,24 @@ package kisiselKullanım;
 public class GitHubKullanımi {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        //1-projeyi git ile irtibatlandırmak için ilk komut "git init" bunu yazınca proje githubla ilişkilendirildi
-        //2-git ile irtibatlandırdıktan sonra benim dosayalarımın git e eklenmesi için "git add .
-        //3-
+        //1-projeyi git ile irtibatlandırmak için ilk komut git init bunu yazınca proje githubla ilişkilendirildi
+        //2-git ile irtibatlandırdıktan sonra benim dosayalarımın git e eklenmesi için git add . yaptım.
+        //3-senkronize etmemiz için versiyon olusturmamız lazım onun için git commit -m "İlkVersion" diyoruz
+        //4-bu 3 komutla lokalde ki işi bitirdik
+
+
+        //Uzak masaüstü için yapılacaklar
+        //1-new den yeni bir repository olusturduk isim aynı olsa güzel olur ama sart degil
+        //2-geri kalan adımları ordan kopyalayıp kendi terminalime yapıstırdım
+        //3-lokalde olusturdugumuz proje uzaktaki masaüstüne senkronize oldu
         //4-
+
+
+        //projede degişiklik yapıp bunu güncelleme nasıl
+        System.out.println("that's it");
+        //bunu nasıl update edecegiz
+        //1.adım artık gitle irtibatli init yok
+        //2.adım dosya ekleme idi
 
     }
 }
